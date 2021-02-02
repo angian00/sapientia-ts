ac95711b5cd83a924d5dcbd2bf0562218fd664ae
angian <andrea.giancola@gmail.com>
2021-02-02 18:42:05 +0100

    Added checks on browser capabilities

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,25 @@ async function gameLoop() {
 }
 
 
-window.setTimeout(gameLoop, 500)
+function isMobileBrowser(): boolean {
+	return ('ontouchstart' in document.documentElement)
+}
+
+const minXRes = 1600
+const minYRes = 800
+
+function isResolutionKO(): boolean {
+	let xRes = window.screen.width * window.devicePixelRatio
+	let yRes = window.screen.height * window.devicePixelRatio
+
+	return (xRes < minXRes || yRes < minYRes) 
+}
+
+if (isMobileBrowser()) {
+	alert("This game can be played only on desktop computers")
+} else if (isResolutionKO()) {
+	alert(`This game can be played only on resolution ${minXRes}x${minYRes} or higher`)
+} else {
+	//ok, let's play
+	window.setTimeout(gameLoop, 500)
+}
